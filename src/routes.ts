a7ae7bb1c96a4c6b9b39a6dcf087e53b3baca313
observer1220 <observer1220@gmail.com>
2024-11-01 15:17:48 +0800

    update at 2024-11-01 15:17:48

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,12 @@ import SearchPage from "./pages/SearchPage.vue";
 import ProfilePage from "./pages/ProfilePage.vue";
 import ProfileEdittingPage from "./pages/ProfileEdittingPage.vue";
 import LoginPage from "./pages/LoginPage.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { getJwtToken } from "./utils/localStorage";
 
 const routes = [
   {
-    path: "/umi",
+    path: "/",
     name: "home",
     component: HomePage,
   },
@@ -41,13 +41,13 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHistory(),
+  history: createWebHashHistory(),
 });
 
 router.beforeEach((to) => {
   // 如果名稱不是login，且沒有jwtToken，就導到login
   if (to.name !== "login" && !getJwtToken()) {
-    return { name: "login" }
+    return { name: "login" };
   }
 
   // 如果名稱是login，且有jwtToken，就導到home
